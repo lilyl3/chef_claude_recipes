df9e07ff9d014cb9fd68aedcc36c76e7235d7f11
lilyl3 <lily.lin.915@gmail.com>
2025-08-09 15:29:03 -0700

    k8s yaml files attempt

--- backend/app.py
+++ backend/app.py
@@ -3,8 +3,6 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
 from huggingface_hub import InferenceClient
-# from transformers import pipeline
-
 
 app = Flask(__name__)
 CORS(app)
